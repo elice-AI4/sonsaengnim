47e2ce3f50d6d000bc30db95a20a463e42e3aa9d
Malza0408 <parkjh04080@gmail.com>
2022-06-03 12:06:29 +0900

    feat : project setting ts,styled-component

---
+++ src/styles/global-styles.ts
@@ -0,0 +1,19 @@
+import { createGlobalStyle, css } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+    * {
+        box-sizing: border-box;
+    }
+    html {
+        font-size: 62.5%;
+    }
+
+    ${({ theme }) => {
+      return css`
+        h1 {
+          font-size: ${theme.text.largest};
+          color: ${theme.colors.h1_color};
+        }
+      `;
+    }}
+`;
---
+++ src/styles/theme.ts
@@ -0,0 +1,20 @@
+// my-theme.ts
+import { DefaultTheme } from "styled-components";
+
+const theme: DefaultTheme = {
+  borderRadius: "5px",
+
+  colors: {
+    main: "cyan",
+    secondary: "magenta",
+    my_red: "red",
+    h1_color: "red",
+    user_color: "blue",
+  },
+
+  text: {
+    largest: "50px",
+  },
+};
+
+export { theme };
