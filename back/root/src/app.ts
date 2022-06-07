3fe3d7daea474d55e0543a9533cc74e578d242a4
DongHyun Kim <developerforus@gmail.com>
2022-06-07 13:57:25 +0900

    [Feat]: morgan add

--- back/root/src/app.ts
+++ back/root/src/app.ts
@@ -2,9 +2,13 @@ import express from "express";
 import { indexRouter } from "./routes/indexRouter";
 import { handRouter } from "./routes/handRouter";
 
+import morgan from "morgan";
+
 import { errorMiddleware } from "./middlewares/errorMiddleware";
 
 const app = express();
+
+app.use(morgan("dev"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
