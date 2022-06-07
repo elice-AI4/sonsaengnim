274334a9afca24c520d9b523ddeeda2d49dc47be
Malza0408 <parkjh04080@gmail.com>
2022-06-08 00:15:42 +0900

    feat : learning route

--- front/src/components/route/route.ts
+++ front/src/components/route/route.ts
@@ -2,13 +2,7 @@ export const ROUTE = {
   LOGIN: {
     link: "login",
   },
-  COUNT: {
-    link: "count",
-  },
-  TEXT: {
-    link: "text",
-  },
-  FC: {
-    link: "fc",
+  LEARNING: {
+    linl: "learning",
   },
 };
