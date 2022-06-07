487bff27b59fa1f36d15f0e9ac20b3065768682d
[4기-2반]김유진 <yujingim@kookmin.ac.kr>
2022-06-07 11:16:42 +0900

    Fix : eslint process 에러 해결

--- back/root/src/middlewares/errorMiddleware.ts
+++ back/root/src/middlewares/errorMiddleware.ts
@@ -1,9 +1,8 @@
-import { Request, Response, NextFunction } from "express";
+import { Request, Response } from "express";
 const errorMiddleware = (
   error: any,
   req: Request,
   res: Response,
-  next: NextFunction
 ) => {
   console.log("\x1b[33m%s\x1b[0m", error);
   res.status(400).send(error.message);
