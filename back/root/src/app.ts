af6b450d0e678eced301a3a2ea1741184859d4ed
DongHyun Kim <developerforus@gmail.com>
2022-06-08 14:18:22 +0900

    [Feat]: Swagger login + cors

--- back/root/src/app.ts
+++ back/root/src/app.ts
@@ -2,8 +2,9 @@ import express from "express";
 import { indexRouter } from "./routes/indexRouter";
 import { handRouter } from "./routes/handRouter";
 import { userRouter } from "./routes/userRouter";
-import morgan from "morgan";
 
+import morgan from "morgan";
+import cors from "cors";
 import path from "path";
 
 // 공통 사항
@@ -20,6 +21,7 @@ const app = express();
 const swaggerYaml = YAML.load(path.join(__dirname, "../../root/build/swagger.yaml"));
 
 app.use(morgan("dev"));
+app.use(cors());
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -29,7 +31,7 @@ app.use(handRouter);
 app.use(userRouter);
 
 // yaml로 된 swagger 연동
-app.use("/api-yaml", swaggerUi.serve, swaggerUi.setup(swaggerYaml));
+app.use("/swagger", swaggerUi.serve, swaggerUi.setup(swaggerYaml));
 
 app.use(errorMiddleware);
 export { app };
