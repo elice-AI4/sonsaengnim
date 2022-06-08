4b591b9587aa6a0bd2ce757df29278f5c02e442d
[4기-2반]김유진 <yujingim@kookmin.ac.kr>
2022-06-08 14:25:16 +0900

    Chore: 주석 추가

--- back/root/src/db/models/Hand.ts
+++ back/root/src/db/models/Hand.ts
@@ -6,11 +6,13 @@ class HandModel {
     return newHand;
   };
 
+  // 수화 데이터 한꺼번에 가져오기
   static findAll = async () =>{
     const newHand = await Hand.find();
     return newHand;
   };
 
+  // 수화 데이터 알파벳별로 가져오기
   static findByAlph = async ({ alphabet }) =>{
     const newHand = await Hand.find({ alphabet: alphabet });
     return newHand;
