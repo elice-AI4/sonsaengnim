07a6f48cd647bd11db94114022299a7deb79c6bb
DongHyun Kim <developerforus@gmail.com>
2022-06-08 11:33:34 +0900

    [Create]: Swagger

--- back/root/src/app.ts
+++ back/root/src/app.ts
@@ -4,9 +4,20 @@ import { handRouter } from "./routes/handRouter";
 import { userRouter } from "./routes/userRouter";
 import morgan from "morgan";
 
+import path from "path";
+
+// 공통 사항
+import swaggerUi from "swagger-ui-express";
+
+// yaml을 연동하기 위함
+import YAML from "yamljs";
+
 import { errorMiddleware } from "./middlewares/errorMiddleware";
 
 const app = express();
+
+// yaml 파일 연동
+const swaggerYaml = YAML.load(path.join(__dirname, "../../root/build/swagger.yaml"));
 
 app.use(morgan("dev"));
 
@@ -16,6 +27,9 @@ app.use(express.json());
 app.use(indexRouter);
 app.use(handRouter);
 app.use(userRouter);
+
+// yaml로 된 swagger 연동
+app.use("/api-yaml", swaggerUi.serve, swaggerUi.setup(swaggerYaml));
 
 app.use(errorMiddleware);
 export { app };
