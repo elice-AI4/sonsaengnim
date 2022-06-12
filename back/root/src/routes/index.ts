8bc587d2adcedc44a1ef9d8ffc5869d15b898b99
DongHyun Kim <developerforus@gmail.com>
2022-06-12 11:35:09 +0900

    [Fix]: IndexRouter

--- back/root/src/routes/index.ts
+++ back/root/src/routes/index.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
 // import { indexRouter } from "./indexRouter";
 import userRouter from "./userRouter";
+import indexRouter from "./indexRouter";
 
 export default () => {
   const app = Router();
 
   userRouter(app);
-
+  indexRouter(app);
   return app;
 };
