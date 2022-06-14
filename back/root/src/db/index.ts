f0627c8789183b6bec9c6457a0773b467790f5dc
elizabethgim <yujingim@kookmin.ac.kr>
2022-06-14 21:10:28 +0900

    Typo: 몬고 -> 몽고

--- back/root/src/db/index.ts
+++ back/root/src/db/index.ts
@@ -10,6 +10,6 @@ const MONGO_URL: any = process.env.MONGO_URL;
 mongoose
   .connect(MONGO_URL)
   .then(() => console.log(`${MONGO_URL} 연결 성공`))
-  .catch(() => console.log("몬고 디비 연결 실패 ㅠㅠ"));
+  .catch(() => console.log("몽고 디비 연결 실패 ㅠㅠ"));
 
 export { HandModel, UserModel };
