10b2caf94906b26b5435c7c7f2cbbbbdfe5aff03
elizabethgim <yujingim@kookmin.ac.kr>
2022-06-14 21:35:22 +0900

    Refactor: UserType -> IUser, IUser에 timestamp를 켜서 createdAt, updatedAt 추가

--- back/root/src/db/schemas/user.ts
+++ back/root/src/db/schemas/user.ts
@@ -1,16 +1,18 @@
 import { model, Schema } from "mongoose";
 
 // 1. Create an interface representing a document in MongoDB.
-interface UserType {
-  name: string;
-  email: string;
-  password: string;
-  errorMessage?;
+export interface IUser {
+  username: string, 
+  email: string,
+  password: string,
+  errorMessage?,
+  createdAt?: Date,
+  updatedAt?: Date
 }
 
 const userSchema = new Schema(
   {
-    name: {
+    username: {
       type: String,
       required: true,
       index: true,
@@ -19,6 +21,7 @@ const userSchema = new Schema(
       type: String,
       unique: true,
       required: true,
+      index: true,
     },
     password: {
       type: String,
@@ -30,6 +33,6 @@ const userSchema = new Schema(
   },
 );
 
-const User = model<UserType>("User", userSchema);
+const User = model<IUser>("User", userSchema);
 
 export { User };
