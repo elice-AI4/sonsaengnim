8f6be906120854002c7c585bce44071f8a354ab3
elizabethgim <yujingim@kookmin.ac.kr>
2022-06-14 21:11:51 +0900

    Fix: 포트 번호 number로 변환

--- back/root/index.ts
+++ back/root/index.ts
@@ -2,7 +2,7 @@ import { app } from "./src/app";
 import dotenv from "dotenv";
 dotenv.config();
 
-const PORT: any = process.env.PORT;
+const PORT: any = +process.env.PORT;
 
 app.listen(PORT, () => {
   console.log(`${PORT}번 포트 온`);
