6ed5fd9981ff3e6dfb84373d28eb88501112d977
elizabethgim <yujingim@kookmin.ac.kr>
2022-06-14 21:38:38 +0900

    Fix: 클래스 handService에서 Handmodel 생성 추가

--- back/root/src/services/handService.ts
+++ back/root/src/services/handService.ts
@@ -1,16 +1,18 @@
-import { HandModel } from "../db/index";
-import { HandType } from "../db/schemas/hand";
+import { HandModel } from "../db";
+import { IHand } from "../db/schemas/hand";
 
 class HandService {
+  constructor(public model:HandModel){};
+
   // 수화 데이터 생성
-  static createHand: Function = async (newHandData: HandType) => {
-    const newHand = await HandModel.create(newHandData);
+  public async createHand(newHandData: IHand) {
+    const newHand = await this.model.create(newHandData);
     return newHand;
   };
 
   // 수화 데이터 알파벳으로 가져오기
-  static getHanddata: Function = async (alphabet: string) => {
-    const newHand = await HandModel.findByAlph({ alphabet: alphabet });
+  public async getHanddata (alphabet: string) {
+    const newHand = await this.model.findByAlph(alphabet);
     return newHand;
   };
 }
