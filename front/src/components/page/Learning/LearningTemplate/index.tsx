fd2bc8b81eaa417a539074585eb717a16edd8d61
Malza0408 <parkjh04080@gmail.com>
2022-06-15 16:25:35 +0900

    fix : 폴더명 및 경로 변경

---
+++ front/src/components/page/Learning/LearningTemplate/index.tsx
@@ -0,0 +1,59 @@
+import React, { useRef } from "react";
+import {
+  Button,
+  ButtonLink,
+  CardContainer,
+  ContentContainer,
+  DescriptionContainer,
+  H1,
+  TemplateContainer,
+} from "./index.style";
+import CardTemplate from "./CardTemplate";
+import { ReactComponent as Underline } from "../../../../src_assets/Underline.svg";
+import { InputContainer } from "../Word/index.style";
+import { motion } from "framer-motion";
+import { useLocation } from "react-router";
+
+interface LearningTemplateProps {
+  imgs: {
+    src: string;
+    alt: string;
+  }[];
+  title: string;
+  children?: React.ReactElement;
+}
+
+const LearningTemplate = ({ imgs, title, children }: LearningTemplateProps) => {
+  const { pathname } = useLocation();
+
+  console.log(pathname);
+
+  return (
+    <TemplateContainer>
+      <ContentContainer>
+        <DescriptionContainer>
+          <H1>{title}</H1>
+          <Underline />
+          {children && <InputContainer>{children}</InputContainer>}
+        </DescriptionContainer>
+        <CardContainer>
+          {imgs.map((img, index) => {
+            return (
+              <CardTemplate src={img.src} alt={img.alt} key={`img ${index}`} />
+            );
+          })}
+
+          <CardTemplate src="" alt="" />
+          <CardTemplate src="" alt="" />
+          <CardTemplate src="" alt="" />
+          <CardTemplate src="" alt="" />
+        </CardContainer>
+        <Button>
+          <ButtonLink to={pathname}>따라해보기</ButtonLink>
+        </Button>
+      </ContentContainer>
+    </TemplateContainer>
+  );
+};
+
+export default LearningTemplate;
