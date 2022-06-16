ba8738a66a6f2a24b7a8111ee3fe9e0db0529d72
elizabethgim <yujingim@kookmin.ac.kr>
2022-06-16 17:38:03 +0900

    Feat: bail을 활용한 추가적인 validation

--- back/root/src/api/middlewares/validators/handValidator.ts
+++ back/root/src/api/middlewares/validators/handValidator.ts
@@ -2,10 +2,18 @@ import { param, body } from "express-validator";
 import { validate } from "./result";
 
 export const handValidate = [
-  body("alphabet").exists().trim().notEmpty().withMessage("알파벳 항목이 없습니다."),
-  body("handImage").exists().trim().notEmpty().withMessage("handImage가 없습니다."), // body("handImage").isURL()
-  body("mouthImage").exists().trim().notEmpty().withMessage("mouthImage가 없습니다."), // body("handImage").isURL()
-  body("video").exists().trim().notEmpty().withMessage("video가 없습니다."), // body("handImage").isURL()
+  body("alphabet").exists().withMessage("알파벳 항목이 없습니다.").bail()
+    .trim().notEmpty().withMessage("공백입니다. 입력된 알파벳이 없습니다.").bail()
+    .isString().withMessage("string 으로 입력해주세요."),
+  body("handImage").exists().withMessage("handImage 항목이 없습니다.").bail()
+    .trim().notEmpty().withMessage("공백입니다. 입력된 handImage가 없습니다."), 
+    // .isURL()
+  body("mouthImage").exists().withMessage("mouthImage 항목이 없습니다.").bail()
+    .trim().notEmpty().withMessage("공백입니다. 입력된 mouthImage가 없습니다.").bail(), 
+    // .isURL()
+  body("video").exists().withMessage("video 항목이 없습니다.").bail()
+    .trim().notEmpty().withMessage("공백입니다. 입력된 video가 없습니다.").bail(), 
+    // .isURL()
   validate,
 ];
 
--- back/root/src/api/middlewares/validators/userValidator.ts
+++ back/root/src/api/middlewares/validators/userValidator.ts
@@ -9,8 +9,12 @@ export const userValidateOptional = [
 ];
 
 export const userValidate = [
-  body("email").exists().trim().isEmail().withMessage("이메일 형식으로 입력하세요."),
-  body("username").exists().trim().notEmpty().withMessage("공백은 안됩니다."),
-  body("password").exists().trim().notEmpty().withMessage("공백은 안됩니다."),
+  body("email").exists().withMessage("email 항목이 없습니다.").bail()
+    .trim().notEmpty().withMessage("공백은 안됩니다. username을 입력해주세요.").bail()
+    .isEmail().withMessage("이메일 형식으로 입력하세요."),
+  body("username").exists().withMessage("username 항목이 없습니다.").bail()
+    .trim().notEmpty().withMessage("공백은 안됩니다. username을 입력해주세요."),
+  body("password").exists().withMessage("password 항목이 없습니다.").bail()
+    .trim().notEmpty().withMessage("공백은 안됩니다. password를 입력해주세요."),
   validate,
 ];
