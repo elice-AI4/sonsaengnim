46f5337973630c95978251f91ed826379d40c65f
DongHyun Kim <developerforus@gmail.com>
2022-06-17 12:29:29 +0900

    [Fix]: folder check

--- front/src/components/page/about/About.style.tsx
+++
@@ -1,9 +0,0 @@
-import styled, { css } from "styled-components";
-
-export const AboutContainer = styled.div`
-  ${({ theme }) => {
-    return css`
-      transform: translateY(${theme.navbar.height});
-    `;
-  }}
-`;
