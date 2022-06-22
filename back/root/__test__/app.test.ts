859640a14c7aab3535aacd59f39ad5800ef85c4a
elizabethgim <yujingim@kookmin.ac.kr>
2022-06-22 16:12:31 +0900

    Feat: testing할 때 유저 정보 랜덤으로 생성

--- back/root/__test__/app.test.ts
+++ back/root/__test__/app.test.ts
@@ -1,9 +1,28 @@
 /* eslint-disable no-undef */
 import app from "../src/app";
-
 import request from "supertest";
+import { v4 as uuid } from "uuid";
 
 describe("End-to-End Test", () => {
+  let random_user;
+
+  beforeAll(() => {
+    random_user = uuid().split("-")[1];
+  });
+
+  describe("POST /register", () => {
+    test("responds with user information in json", async () => {
+      await request(app)
+        .post("/register")
+        .set("Accept", "application/json")
+        .type("application/json")
+        .send({ username: "test" + random_user, email: random_user + "@test.com", password: "12341234" })
+        .expect(200)
+        .expect("Content-Type", /json/);
+      //.expect({ name: "modolee" });
+    });
+  });
+
   describe("GET /", () => {
     test("responds with json", async () => {
       await request(app)
@@ -15,26 +34,13 @@ describe("End-to-End Test", () => {
     });
   });
 
-  describe("POST /register", () => {
-    test("responds with user information in json", async () => {
-      await request(app)
-        .post("/register")
-        .set("Accept", "application/json")
-        .type("application/json")
-        .send({ username: "kim", email: "test123123123@test.com", password: "1234" })
-        .expect(200)
-        .expect("Content-Type", /json/);
-      //.expect({ name: "modolee" });
-    });
-  });
-
   describe("POST /user", () => {
     test("user login", async () => {
       await request(app)
         .post("/user")
         .set("Accept", "application/json")
         .type("application/json")
-        .send({ email: "test123123123@test.com", password: "1234" })
+        .send({ email: random_user + "@test.com", password: "12341234" })
         .expect(200)
         .expect("Content-Type", /json/);
       //.expect({ name: "modolee" });
