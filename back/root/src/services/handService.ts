35138ee43b74ca01cdc44f837c20880b77fa1fb7
[4기-2반]김유진 <yujingim@kookmin.ac.kr>
2022-06-25 23:03:07 +0900

    Refactor: Unnecessary try/catch wrapper 지우기

--- back/root/src/services/handService.ts
+++ back/root/src/services/handService.ts
@@ -5,38 +5,26 @@ export default class HandService {
   constructor(private handModel: MongoHandModel) {}
 
   public async get(alphabet: string) {
-    try {
-      const hand = await this.handModel.findByAlphabet(alphabet);
-      if (hand.length === 0) {
-        throw new Error("해당 알파벳 데이터를 찾을 수 없습니다.");
-      }
-      return hand;
-    } catch (error) {
-      throw error;
+    const hand = await this.handModel.findByAlphabet(alphabet);
+    if (hand.length === 0) {
+      throw new Error("해당 알파벳 데이터를 찾을 수 없습니다.");
     }
+    return hand;
   }
 
   async getAll() {
-    try {
-      const hand = await this.handModel.findAll();
-      if (hand.length === 0) {
-        throw new Error("수화 데이터가 존재하지 않습니다.");
-      }
-      return hand;
-    } catch (error) {
-      throw error;
+    const hand = await this.handModel.findAll();
+    if (hand.length === 0) {
+      throw new Error("수화 데이터가 존재하지 않습니다.");
     }
+    return hand;
   }
 
   async create(newHandData: IHand) {
-    try {
-      const newHand = await this.handModel.create(newHandData);
-      if (!newHand) {
-        throw new Error("수화 데이터 생성에 실패했습니다.");
-      }
-      return newHand;
-    } catch (error) {
-      throw error;
+    const newHand = await this.handModel.create(newHandData);
+    if (!newHand) {
+      throw new Error("수화 데이터 생성에 실패했습니다.");
     }
+    return newHand;
   }
 }
