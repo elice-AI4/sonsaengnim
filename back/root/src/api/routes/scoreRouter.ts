30b7f0f979883d17bbad7f024ab4f87de03cfded
[4기-2반]김유진 <yujingim@kookmin.ac.kr>
2022-06-26 13:09:57 +0900

    Feat: getTopten 함수 작성 (미완)

--- back/root/src/api/routes/scoreRouter.ts
+++ back/root/src/api/routes/scoreRouter.ts
@@ -10,8 +10,8 @@ scoreRouter.get("/", checkLogin, async (req, res, next) => {
   try {
     const { score } = req.body;
     const userId: string = req.user;
-    let scoreObject = {};
-    const scoreBoard = await scoreService.addScore(userId, score);
+
+    const scoreBoard = await scoreService.addScore({ userId, score });
     res.status(200).send(scoreBoard);
   } catch (error) {
     next(error);
@@ -20,7 +20,8 @@ scoreRouter.get("/", checkLogin, async (req, res, next) => {
 
 scoreRouter.get("/topten", async (req, res, next) => {
   try {
-    res.status(200).send("Top 10");
+    const scoreBoard = await scoreService.getTopten();
+    res.status(200).send(scoreBoard);
   } catch (error) {
     next(error);
   }
--- back/root/src/db/models/Score.ts
+++ back/root/src/db/models/Score.ts
@@ -2,7 +2,23 @@ import Score from "../schemas/score";
 import { IScore, IScoreModel } from "../../models";
 export class MongoScoreModel implements IScoreModel {
   async createScore(scoreData: IScore) {
-    const score = await Score.create(scoreData);
+    const existUser = await Score.find({ userId: scoreData.userId });
+    let score;
+    if (existUser.length === 0) {
+      score = await Score.create(scoreData);
+    } else {
+      const mostScored = existUser.sort().reverse()[0];
+      if (mostScored.score <= scoreData.score) {
+        await Score.deleteMany({ userId: scoreData.userId });
+        score = await Score.create(scoreData);
+      }
+    }
+
     return score;
+  }
+
+  async getTopten() {
+    // 동점자 처리 필요
+    return await Score.find().sort({ created_at: -1 }).sort({ score: -1 });
   }
 }
