9a9c4f721a59aaa4198b556888c34759da68f07f
[4기-2반]김유진 <yujingim@kookmin.ac.kr>
2022-06-26 11:24:04 +0900

    Feat: scoreService

---
+++ back/root/src/services/scoreService.ts
@@ -0,0 +1,15 @@
+import { MongoScoreModel } from "../db/models/Score";
+import { IScore } from "../models";
+
+export default class ScoreService {
+  constructor(private scoreModel: MongoScoreModel) {}
+
+  async addScore(scoreData: IScore) {
+    const newScore = await this.scoreModel.createScore(scoreData);
+    return newScore;
+  }
+
+  async getTopten() {
+    return "success";
+  }
+}
