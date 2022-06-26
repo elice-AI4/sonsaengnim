e2ecbb8f7c4ac6b70fb142a3350672d93519ccfc
[4기-2반]김유진 <yujingim@kookmin.ac.kr>
2022-06-26 11:24:29 +0900

    Feat: scoreRouter 추가

--- back/root/src/api/index.ts
+++ back/root/src/api/index.ts
@@ -4,6 +4,7 @@ import { registerRouter } from "./routes/registerRouter";
 import { indexRouter } from "./routes/indexRouter";
 import { userRouter } from "./routes/userRouter";
 import { handRouter } from "./routes/handRouter";
+import { scoreRouter } from "./routes/scoreRouter";
 
 const router = Router();
 
@@ -11,5 +12,6 @@ router.use("/", indexRouter);
 router.use("/register", registerRouter);
 router.use("/user", userRouter);
 router.use("/hands", handRouter);
+router.use("/scores", scoreRouter);
 
 export { router };
