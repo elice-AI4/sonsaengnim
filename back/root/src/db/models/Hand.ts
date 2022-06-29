679db538dc016ce43253bc2a4cf752f28a4d1b79
[4기-2반]김유진 <yujingim@kookmin.ac.kr>
2022-06-29 21:48:27 +0900

    Refactor: change _id to id

--- back/root/src/db/models/Hand.ts
+++ back/root/src/db/models/Hand.ts
@@ -2,6 +2,8 @@ import { IHandModel } from "../../models";
 import { IHand } from "../../models";
 
 import { Hand } from "../schemas/hand";
+// mongoose findByIddml ruddn
+// ObjectId 형식 체킹
 
 class MongoHandModel implements IHandModel {
   public async create(newHandData: IHand) {
@@ -17,17 +19,19 @@ class MongoHandModel implements IHandModel {
 
   // 수화 데이터 알파벳별로 가져오기
   public async findByEnglish(english: string) {
-    const newHand = await Hand.find({ english }).lean();
+    const newHand = await Hand.findOne({ english }).lean();
     return newHand;
   }
 
   public async update({ id, toUpdate }: { id: string; toUpdate: Partial<IHand> }) {
+    // type validation 필요 없음.
     const newHand = await Hand.findByIdAndUpdate(id, { $set: toUpdate });
     return newHand;
   }
 
-  public async delete(_id: string) {
-    const result = await Hand.findByIdAndDelete(_id);
+  public async delete(id: string) {
+    // Objectid 체크
+    const result = await Hand.findByIdAndDelete(id);
     return result;
   }
 }
