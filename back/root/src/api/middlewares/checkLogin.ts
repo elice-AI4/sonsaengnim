14407ab819b1700c93899ad0990ff80982fc86e4
DongHyun Kim <developerforus@gmail.com>
2022-06-29 20:46:31 +0900

    [Refactor]: Token with Bearer

--- back/root/src/api/middlewares/checkLogin.ts
+++ back/root/src/api/middlewares/checkLogin.ts
@@ -13,10 +13,17 @@ const checkLogin = (req: Request, res: Response, next: NextFunction) => {
       });
     } else {
       // 토큰 변형 확인
-      const token = req.headers["authorization"].split(" ")[1];
-      const decoded = jwt.verify(token, config.JWT_KEY) as jwt.JwtPayload;
-      req.user = decoded.ObjectId;
-      next();
+      const bearerToken = req.headers["authorization"].split(" ");
+      if (bearerToken.length !== 2) {
+        next(new Error("Bearer 문구와 함께 token을 보내주세요!"));
+      } else if (bearerToken[0] !== "Bearer") {
+        next(new Error("Bearer 문구와 함께 token을 보내주세요"));
+      } else {
+        const token = bearerToken[1];
+        const decoded = jwt.verify(token, config.JWT_KEY) as jwt.JwtPayload;
+        req.user = decoded.ObjectId;
+        next();
+      }
     }
   } catch (error) {
     res.status(401).json({
