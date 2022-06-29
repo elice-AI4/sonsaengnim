4dd386a2f3165492df97dd8cb1e38091ec3b5da2
[4기-2반]김유진 <yujingim@kookmin.ac.kr>
2022-06-29 21:35:04 +0900

    Feat: check token method

--- back/root/src/api/middlewares/checkLogin.ts
+++ back/root/src/api/middlewares/checkLogin.ts
@@ -13,6 +13,15 @@ const checkLogin = (req: Request, res: Response, next: NextFunction) => {
       });
     } else {
       // 토큰 변형 확인
+      // bearer 확인
+      const bearer = req.headers["authorization"].split(" ")[0];
+      console.log(bearer);
+      if (bearer !== "Bearer") {
+        res.status(401).json({
+          status: "fail",
+          message: "Method must be 'Bearer'",
+        });
+      }
       const token = req.headers["authorization"].split(" ")[1];
       const decoded = jwt.verify(token, config.JWT_KEY) as jwt.JwtPayload;
       req.user = decoded.ObjectId;
