9608bd0abc8a5423514f893e067c85e2644e13ef
[4기-2반]김유진 <yujingim@kookmin.ac.kr>
2022-06-29 15:10:44 +0900

    Feat: score router 추가

--- back/root/src/api/routes/scoreRouter.ts
+++ back/root/src/api/routes/scoreRouter.ts
@@ -1,28 +1,31 @@
 import { Router } from "express";
-import checkLoginForScore from "../middlewares/checkLoginForScore";
 import { MongoScoreModel, MongoUserModel } from "../../db";
 import ScoreService from "../../services/scoreService";
+import checkLogin from "../middlewares/checkLogin";
 
 const scoreRouter = Router();
 const scoreService = new ScoreService(new MongoScoreModel());
 const userModel = new MongoUserModel();
 
-scoreRouter.post("/", checkLoginForScore, async (req, res, next) => {
+// login User
+scoreRouter.post("/login", checkLogin, async (req, res, next) => {
   try {
     const { score, time } = req.body;
-    const user = req.user;
-    const userId = req.userId;
-    let scoreBoard;
+    const userId = req.user;
+    const registeredUser = await userModel.findById(userId);
+    console.log("login: ", registeredUser);
+    const scoreBoard = await scoreService.addScore({ username: registeredUser.username, score, time, userId: userId });
+    res.status(201).send(scoreBoard);
+  } catch (error) {
+    next(error);
+  }
+});
 
-    if (typeof user === "string") {
-      // Non-login User
-      scoreBoard = await scoreService.addScore({ username: user, score, time });
-    } else {
-      // login User
-      const registeredUser = await userModel.findById(userId);
-      console.log("login: ", registeredUser);
-      scoreBoard = await scoreService.addScore({ username: registeredUser.username, score, time, userId: userId });
-    }
+// non login user
+scoreRouter.post("/nonlogin", async (req, res, next) => {
+  try {
+    const { username, score, time } = req.body;
+    const scoreBoard = await scoreService.addScore({ username, score, time });
     res.status(201).send(scoreBoard);
   } catch (error) {
     next(error);
