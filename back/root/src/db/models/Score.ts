65408b7e5874ae7069525a84cb51d917ca0f38a2
[4기-2반]김유진 <yujingim@kookmin.ac.kr>
2022-06-29 13:40:56 +0900

    Refactor: user 점수 넣는 거 주석처리

--- back/root/src/db/models/Score.ts
+++ back/root/src/db/models/Score.ts
@@ -1,17 +1,20 @@
 import Score from "../schemas/score";
+// import { MongoUserModel } from "./User";
 import { IScore, IScoreModel } from "../../models";
+
 export class MongoScoreModel implements IScoreModel {
   async createScore(scoreData: IScore) {
-    const existUser = await Score.find({ userId: scoreData.userId });
+    const existUser = await Score.find({ username: scoreData.username });
     let score;
     if (existUser.length !== 0) {
       const mostScored = existUser.sort().reverse()[0];
       if (mostScored.score <= scoreData.score) {
-        await Score.deleteMany({ userId: scoreData.userId });
+        await Score.deleteMany({ username: scoreData.username });
       }
     }
     score = await Score.create(scoreData);
-    console.log("score: ", score);
+    // const userModel = new MongoUserModel();
+    // userModel.pushScore(scoreData.userId);
     return score;
   }
 
