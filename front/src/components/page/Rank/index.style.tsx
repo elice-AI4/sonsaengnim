1d7690ba364f39f83103c718a251bd9a500392f0
BoSeon Park <bedurgi999@gmail.com>
2022-06-30 14:21:42 +0900

    feat:랭킹 보드 수정

--- front/src/components/page/Rank/index.style.tsx
+++ front/src/components/page/Rank/index.style.tsx
@@ -3,16 +3,17 @@ import styled from "styled-components";
 export const RankBox = styled.div`
   width: 100%;
   height: 100vh;
-  border: 1px solid red;
   display: flex;
   flex-direction: column;
   align-items: center;
-  padding-top: 65px;
+  padding-top: 40px;
 `;
 
 export const TropyImg = styled.img`
   width: 200px;
   height: 200px;
+  position: relative;
+  top: 90px;
 `;
 
 export const RankBoard = styled.div<{ rankBoardImg: string }>`
@@ -21,9 +22,9 @@ export const RankBoard = styled.div<{ rankBoardImg: string }>`
   background-size: cover;
   width: 1000px;
   height: 670px;
-  border: 1px solid blue;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  position: relative;
 `;
