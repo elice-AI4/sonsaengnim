138858492545a23f10f715ac064a8928fbd044d9
DongHyun Kim <developerforus@gmail.com>
2022-06-30 12:25:50 +0900

    [Refactor]: async => public async

--- back/root/src/db/models/User.ts
+++ back/root/src/db/models/User.ts
@@ -1,12 +1,12 @@
 import User from "../schemas/user";
 import { IUserModel } from "../../models";
 export class MongoUserModel implements IUserModel {
-  async studyList(userId: string) {
+  public async studyList(userId: string) {
     const studyList = await User.findById(userId, { _id: 0, study: 1 });
     return studyList;
   }
 
-  async study(userId: string, word: string) {
+  public async study(userId: string, word: string) {
     const user = await User.findById(userId);
     if (user.study.indexOf(word) !== -1) {
       throw new Error("이미 학습한 데이터 입니다.");
@@ -17,32 +17,32 @@ export class MongoUserModel implements IUserModel {
     }
   }
 
-  async createUser(userData) {
+  public async createUser(userData) {
     const user = await User.create(userData);
     return user;
   }
 
-  async updateUser(userId, updateUserData) {
+  public async updateUser(userId, updateUserData) {
     const user = await User.findByIdAndUpdate(userId, { $set: updateUserData }, { new: true });
     return user;
   }
 
-  async deleteUser(userId: string) {
+  public async deleteUser(userId: string) {
     const user = await User.findByIdAndDelete(userId);
     return user;
   }
 
-  async findByEmail(email: string) {
+  public async findByEmail(email: string) {
     const user = await User.findOne({ email }).lean();
     return user;
   }
 
-  async findById(userId: string) {
+  public async findById(userId: string) {
     const user = await User.findById(userId).lean();
     return user;
   }
 
-  async pushScore(userId: string, newScore: number, newTime: number) {
+  public async pushScore(userId: string, newScore: number, newTime: number) {
     const user = await User.findById(userId).lean();
     const updatedUser = await User.findByIdAndUpdate(
       userId,
