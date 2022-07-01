65704dea0e3f210bd590720309d8e8eb2354adb7
elizabethgim <yujingim@kookmin.ac.kr>
2022-07-01 20:11:41 +0900

    fix: back https 적용 삭제

--- back/root/index.ts
+++ back/root/index.ts
@@ -1,6 +1,28 @@
 import app from "./src/app";
 import config from "./src/config";
+import mkcert from "mkcert";
+import https from "https";
 
-app.listen(config.PORT, () => {
-  console.log(`${config.PORT}번 포트 온`);
-});
+const server = async () => {
+  // create a certificate authority
+  const ca = await mkcert.createCA({
+    organization: "Hello CA",
+    countryCode: "NP",
+    state: "Bagmati",
+    locality: "Kathmandu",
+    validityDays: 365,
+  });
+
+  // then create a tls certificate
+  const cert = await mkcert.createCert({
+    domains: ["127.0.0.1", "localhost"],
+    validityDays: 365,
+    caKey: ca.key,
+    caCert: ca.cert,
+  });
+
+  https.createServer(cert, app).listen(config.PORT, () => {
+    console.log(`${config.PORT}번 포트 온`);
+  });
+};
+server();
