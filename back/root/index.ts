643a2efe86aac1fa4b8b2fef3b9901e86179b389
DongHyun Kim <developerforus@gmail.com>
2022-07-06 18:25:45 +0900

    [Build]: Mongoose Connect Move Folder

--- back/root/index.ts
+++ back/root/index.ts
@@ -1,6 +1,13 @@
 import app from "./src/app";
+import mongoose from "mongoose";
 import config from "./src/config";
 
-app.listen(config.PORT, () => {
-  console.log(`${config.PORT}번 포트 온`);
-});
+mongoose
+  .connect(config.MONGO_URL)
+  .then(() => {
+    console.log(`${config.MONGO_URL} 연결 성공`);
+    app.listen(config.PORT, () => {
+      console.log(`${config.PORT}번 포트 온`);
+    });
+  })
+  .catch(() => console.log("몽고 디비 연결 실패"));
--- back/root/src/db/index.ts
+++ back/root/src/db/index.ts
@@ -1,12 +1,4 @@
-import mongoose from "mongoose";
-import config from "../config";
-
 export * from "./models/User";
 export * from "./models/Hand";
 export * from "./models/Score";
 export * from "./models/Quiz";
-
-mongoose
-  .connect(config.MONGO_URL)
-  .then(() => console.log(`${config.MONGO_URL} 연결 성공`))
-  .catch(() => console.log("몽고 디비 연결 실패"));
