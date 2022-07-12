74b42f296c0f39c996bbdfa31a2486d177186607
blcklamb <whenucan35@gmail.com>
2022-07-12 16:39:30 +0900

    Fix: guide picture change in alphabet and word

--- front/src/components/page/Learning/Game/index.tsx
+++ front/src/components/page/Learning/Game/index.tsx
@@ -42,7 +42,8 @@ import grading from "../../../../src_assets/modal/grading.jpg";
 import none_user_correct from "../../../../src_assets/modal/none_user_correct.jpg";
 import user_correct from "../../../../src_assets/modal/user_correct.jpg";
 import wrong_answer from "../../../../src_assets/modal/wrong_answer.jpg";
-import playGuide from "../../../../src_assets/learning/play/playGuide.jpg";
+import playAlphabetGuide from "../../../../src_assets/learning/play/playAlphabetGuide.jpg";
+import playWordGuide from "../../../../src_assets/learning/play/playWordGuide.jpg";
 import { useAtom } from "jotai";
 import { loginAtom } from "../../../../state";
 
@@ -549,13 +550,61 @@ const LearningGame = () => {
                 handleSetSocketAnswer={handleSetSocketAnswer}
                 openModal={openModal}
               />
-              <StartButton
+              {isAlphabetLearningPage ? (
+                <StartButton
+                  onClick={handleClickButton}
+                  cameraOn={cameraOn}
+                  data-tip="game-alphabetGuide"
+                  data-for="game-alphabetGuide"
+                >
+                  <StartTriangle cameraOn={cameraOn} />
+
+                  <ReactTooltip id="game-alphabetGuide">
+                    <ToolTipContent>
+                      <img
+                        src={playAlphabetGuide}
+                        alt="playGuide"
+                        width="300"
+                      ></img>
+                      <p style={{ textAlign: "center", fontSize: "24px" }}>
+                        그림처럼 얼굴과 어깨와 손이 <br />
+                        함께 나오도록 자세를 잡아주세요
+                      </p>
+                    </ToolTipContent>
+                  </ReactTooltip>
+                </StartButton>
+              ) : (
+                <StartButton
+                  onClick={handleClickButton}
+                  cameraOn={cameraOn}
+                  data-tip="game-wordGuide"
+                  data-for="game-wordGuide"
+                >
+                  <StartTriangle cameraOn={cameraOn} />
+
+                  <ReactTooltip id="game-wordGuide">
+                    <ToolTipContent>
+                      <img
+                        src={playWordGuide}
+                        alt="playGuide"
+                        width="300"
+                      ></img>
+                      <p style={{ textAlign: "center", fontSize: "24px" }}>
+                        그림처럼 얼굴과 어깨와 양손이 <br />
+                        함께 나오도록 자세를 잡아주세요
+                      </p>
+                    </ToolTipContent>
+                  </ReactTooltip>
+                </StartButton>
+              )}
+              {/* <StartButton
                 onClick={handleClickButton}
                 cameraOn={cameraOn}
                 data-tip="game-guide"
                 data-for="game-guide"
               >
                 <StartTriangle cameraOn={cameraOn} />
+
                 <ReactTooltip id="game-guide">
                   <ToolTipContent>
                     <img src={playGuide} alt="playGuide" width="300"></img>
@@ -565,7 +614,7 @@ const LearningGame = () => {
                     </p>
                   </ToolTipContent>
                 </ReactTooltip>
-              </StartButton>
+              </StartButton> */}
             </BottomContainer>
           </Moniter>
           <Footer
