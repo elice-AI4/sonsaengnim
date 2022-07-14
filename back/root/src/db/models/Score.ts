90cdf5440276583eb0b4fc277e14cfc88ae8a0d0
elizabethgim <yujingim@kookmin.ac.kr>
2022-07-14 16:09:35 +0900

    Fix: start rank from 1

--- back/root/src/db/models/Score.ts
+++ back/root/src/db/models/Score.ts
@@ -18,6 +18,7 @@ export class MongoScoreModel implements IScoreModel {
   public async getTopten() {
     let scores = await Score.aggregate([{ $sort: { score: -1, time: 1 } }, { $addFields: { rank: 0 } }]);
     let result = [];
+    scores[0].rank = 1;
     result.push(scores[0]);
     for (let i = 1, add = 1; i < scores.length; i++) {
       if (scores[i].score == scores[i - 1].score) {
